910b1536bc7243db512357146a71907a64451f27
SeanWiz <triclopszx@gmail.com>
2015-05-03 15:19:47 -0400

    The goat rises, I guess

---
+++ Assets/SetFacing.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetFacing : MonoBehaviour {
+
+	public bool defaultIsRight;
+	public bool faceRight;
+
+	// Use this for initialization
+	void Start () {
+		if (defaultIsRight != faceRight)
+			transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
