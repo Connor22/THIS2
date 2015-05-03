ed99da1ab74881f7cdbd148eeb1880b3bdafb78f
SeanWiz <triclopszx@gmail.com>
2015-05-03 15:53:48 -0400

    Turret directions and bullet stopping

--- Assets/Scripts/Enemy/EnemyFire.cs
+++ Assets/Scripts/Enemy/EnemyFire.cs
@@ -9,16 +9,26 @@ public class EnemyFire : MonoBehaviour {
 	public float waitUntilFire;
 	public float delayBetweenShots;
 
+	private SetFacing direction;
+	
+
 	// Update is called once per frame
 	void Awake () {
+		direction = GetComponent<SetFacing>();
 		StartCoroutine(fireWait (waitUntilFire));
 	}
 
 	void fire(){
 		Vector3 tempVec = transform.position;
-		tempVec.x -= ammoDistanceFromShooter;
+		int facing = 1;
+		if(direction.faceRight){
+			tempVec.x += ammoDistanceFromShooter;
+			facing = -1;
+		} else {
+			tempVec.x -= ammoDistanceFromShooter;
+		}
 		GameObject ammoInstance = (GameObject)Instantiate(ammo, tempVec, transform.rotation);
-		ammoInstance.GetComponent<ForwardShot>().SendMessage("setDirection", 1);
+		ammoInstance.GetComponent<ForwardShot>().SendMessage("setDirection", facing);
 		Vector3 ammoscale = ammoInstance.transform.localScale;
 		ammoscale.x = ammoscale.y = ammoSize;
 		ammoInstance.transform.localScale = ammoscale;
