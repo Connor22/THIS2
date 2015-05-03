5ce234edaac5bd023cb35329d2bb88a3a1a5abe9
Connor22 <con_christopher@hotmail.com>
2015-05-03 17:27:14 -0400

    MapLevel touch-up, start up script

---
+++ Assets/Scripts/StartUp.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		GetComponent<PlayerChecks>().quitProcess();
+	}
+}
